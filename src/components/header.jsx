5f28691897994b36baa3f59fe82b8f10c7d020b7
RaiSufian <mashancs571@gmail.com>
2024-11-13 16:27:56 +0500

    set the menu of the mobile after header animation

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -22,52 +22,55 @@ const Header = () => {
 
 
     return (
-        <div className={`bg-white sticky top-0 z-50 transition-all duration-200 ${isSticky ? 'bg-opacity-60 rounded-b-3xl scale-90' : ''}`}>
-            <div className={`lg:container  flex mx-auto py-3 justify-between items-center px-3 md:px-2 lg:px-6 ${isSticky ? '' : ''}`}>
-                <Link to="/" className="lg:mr-4">
-                    <img src='/logo.png' alt="logo" className="w-24 lg:w-36" />
-                </Link>
-                <div className="main_menu md:block hidden">
-                    <ul className="flex items-center gap-5 lg:gap-12 font-semibold font-sans">
-                        <li className="text-blue-700 relative group py-2">
-                            <Link to="/">HOME</Link>
-                            <span class="absolute left-0 bottom-0 w-0 h-[2px] bg-blue-700 transition-all duration-300 group-hover:w-full"></span>
+        <>
+            <div className={`bg-white sticky top-0 z-50 transition-all duration-200 ${isSticky ? 'bg-opacity-60 rounded-b-3xl scale-90' : ''}`}>
+                <div className={`lg:container  flex mx-auto py-3 justify-between items-center px-3 md:px-2 lg:px-6 ${isSticky ? '' : ''}`}>
+                    <Link to="/" className="lg:mr-4">
+                        <img src='/logo.png' alt="logo" className="w-24 lg:w-36" />
+                    </Link>
+                    <div className="main_menu md:block hidden">
+                        <ul className="flex items-center gap-5 lg:gap-12 font-semibold font-sans">
+                            <li className="text-blue-700 relative group py-2">
+                                <Link to="/">HOME</Link>
+                                <span class="absolute left-0 bottom-0 w-0 h-[2px] bg-blue-700 transition-all duration-300 group-hover:w-full"></span>
 
-                        </li>
-                        <li className="hover:text-blue-500 relative group py-2">
-                            <Link to="/">ABOUT US</Link>
-                            <span class="absolute left-0 bottom-0 w-0 h-[2px] bg-blue-700 transition-all duration-300 group-hover:w-full"></span>
-                        </li>
-                        <li className="hover:text-blue-500 relative group py-2">
-                            <Link to="/">FAQs</Link>
-                            <span class="absolute left-0 bottom-0 w-0 h-[2px] bg-blue-700 transition-all duration-300 group-hover:w-full"></span>
-                        </li>
-                        <li className="hover:text-blue-500 relative group py-2">
-                            <div className="flex gap-1 items-center">
+                            </li>
+                            <li className="hover:text-blue-500 relative group py-2">
+                                <Link to="/">ABOUT US</Link>
+                                <span class="absolute left-0 bottom-0 w-0 h-[2px] bg-blue-700 transition-all duration-300 group-hover:w-full"></span>
+                            </li>
+                            <li className="hover:text-blue-500 relative group py-2">
+                                <Link to="/">FAQs</Link>
+                                <span class="absolute left-0 bottom-0 w-0 h-[2px] bg-blue-700 transition-all duration-300 group-hover:w-full"></span>
+                            </li>
+                            <li className="hover:text-blue-500 relative group py-2">
+                                <div className="flex gap-1 items-center">
 
-                                <Link to="/">OUR SERVICES</Link>
-                                <svg className="text-gray-400" xmlns="http://www.w3.org/2000/svg" width="1.25em" height="0.75em" viewBox="0 0 16 7"><path fill="currentColor" d="M8 6.5a.47.47 0 0 1-.35-.15l-4.5-4.5c-.2-.2-.2-.51 0-.71s.51-.2.71 0l4.15 4.15l4.14-4.14c.2-.2.51-.2.71 0s.2.51 0 .71l-4.5 4.5c-.1.1-.23.15-.35.15Z" /></svg>
-                            </div>
+                                    <Link to="/">OUR SERVICES</Link>
+                                    <svg className="text-gray-400" xmlns="http://www.w3.org/2000/svg" width="1.25em" height="0.75em" viewBox="0 0 16 7"><path fill="currentColor" d="M8 6.5a.47.47 0 0 1-.35-.15l-4.5-4.5c-.2-.2-.2-.51 0-.71s.51-.2.71 0l4.15 4.15l4.14-4.14c.2-.2.51-.2.71 0s.2.51 0 .71l-4.5 4.5c-.1.1-.23.15-.35.15Z" /></svg>
+                                </div>
 
-                            <span class="absolute left-0 bottom-0 w-0 h-[2px] bg-blue-700 transition-all duration-300 group-hover:w-full"></span>
-                        </li>
-                        <li className="hover:text-blue-500 relative group py-2">
-                            <Link to="/">CONTACT US</Link>
-                            <span class="absolute left-0 bottom-0 w-0 h-[2px] bg-blue-700 transition-all duration-300 group-hover:w-full"></span>
-                        </li>
-                    </ul>
+                                <span class="absolute left-0 bottom-0 w-0 h-[2px] bg-blue-700 transition-all duration-300 group-hover:w-full"></span>
+                            </li>
+                            <li className="hover:text-blue-500 relative group py-2">
+                                <Link to="/">CONTACT US</Link>
+                                <span class="absolute left-0 bottom-0 w-0 h-[2px] bg-blue-700 transition-all duration-300 group-hover:w-full"></span>
+                            </li>
+                        </ul>
+                    </div>
+                    <div>
+                        <button className="text-white md:block hidden rounded-full py-2 lg:py-3 px-6 lg:px-10 btn_grad">
+                            Sign Up
+                        </button>
+                    </div>
+                    <div className="md:hidden">
+                        <svg onClick={() => setIsOpen(true)} xmlns="http://www.w3.org/2000/svg" width="2.25em" height="2.25em" viewBox="0 0 24 24"><path fill="currentColor" d="M4 18h16c.55 0 1-.45 1-1s-.45-1-1-1H4c-.55 0-1 .45-1 1s.45 1 1 1m0-5h16c.55 0 1-.45 1-1s-.45-1-1-1H4c-.55 0-1 .45-1 1s.45 1 1 1M3 7c0 .55.45 1 1 1h16c.55 0 1-.45 1-1s-.45-1-1-1H4c-.55 0-1 .45-1 1" /></svg>
+                    </div>
                 </div>
-                <div>
-                    <button className="text-white md:block hidden rounded-full py-2 lg:py-3 px-6 lg:px-10 btn_grad">
-                        Sign Up
-                    </button>
-                </div>
-                <div className="md:hidden">
-                    <svg onClick={() => setIsOpen(true)} xmlns="http://www.w3.org/2000/svg" width="2.25em" height="2.25em" viewBox="0 0 24 24"><path fill="currentColor" d="M4 18h16c.55 0 1-.45 1-1s-.45-1-1-1H4c-.55 0-1 .45-1 1s.45 1 1 1m0-5h16c.55 0 1-.45 1-1s-.45-1-1-1H4c-.55 0-1 .45-1 1s.45 1 1 1M3 7c0 .55.45 1 1 1h16c.55 0 1-.45 1-1s-.45-1-1-1H4c-.55 0-1 .45-1 1" /></svg>
-                </div>
+
             </div>
             <div
-                className={`md:hidden absolute z-50 bg-white p-10 transition-all duration-300 top-0 bottom-0 ${isOpen ? 'translate-x-0 opacity-100 w-full left-0' : 'translate-x-full opacity-0 w-0'
+                className={`md:hidden fixed z-50 bg-white p-10 transition-all duration-300 ${isOpen ? 'translate-x-0 opacity-100 top-0 left-0 h-screen w-screen' : 'translate-x-full opacity-0 w-0'
                     }`}>
                 <div >
                     <svg onClick={() => setIsOpen(false)} className="ml-auto" xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" viewBox="0 0 48 48"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="4" d="m8 8l32 32M8 40L40 8" /></svg>
@@ -114,7 +117,7 @@ const Header = () => {
                     </li>
                 </ul>
             </div>
-        </div>
+        </>
     )
 }
 export default Header;
--- src/pages/homePage/index.jsx
+++ src/pages/homePage/index.jsx
@@ -11,7 +11,7 @@ import Footer from "../../components/footer";
 const Home = () => {
     return (
 
-        <div className="relative ">
+        <div className=" ">
             <Topbar />
              <Header />
            < Hero />
